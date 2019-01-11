e92606be7c4e23a06e6f2e252cf133a0321c8122
dropecosta <pedro-reis@puc.br>
2019-01-11 17:37:12 -0200

    creating register of user with mutation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApolloConfigModule } from './apollo-config.module';
 
 @NgModule({
   declarations: [
@@ -10,10 +11,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserAnimationsModule,
+    ApolloConfigModule,
     MatListModule,
     MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
