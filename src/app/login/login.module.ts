af2493ac413bbda489a5e91b216bca3b71c2b695
dropecosta <pedro-reis@puc.br>
2019-01-17 15:09:07 -0200

    generating LoginModule and LoginRoutingModule and organizing AppComponent and CoreModule

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
