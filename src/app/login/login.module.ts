ebf8ceff6a1ffece60446ad2e31f83206b2056ef
dropecosta <pedro-reis@puc.br>
2019-01-17 17:32:08 -0200

    implementing and validating reactive forms for login.component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     SharedModule,
     LoginRoutingModule
-  ]
+  ],
+  exports: [LoginComponent]
 })
 export class LoginModule { }
