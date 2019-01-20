6479fd2a1df311eac7d876433cf11152247d42fa
Pedro Costa <dropecosta@gmail.com>
2019-01-20 16:17:51 -0200

    method for signup user on API graphql

--- src/app/core/services/auth.graphql.ts
+++ src/app/core/services/auth.graphql.ts
@@ -1,8 +1,17 @@
 import gql from 'graphql-tag';
 
- export const AUTHENTICATE_USER_MUTATION = gql`
+export const AUTHENTICATE_USER_MUTATION = gql`
   mutation AuthenticateUserMutation($email: String!, $password: String!) {
     authenticateUser(email: $email, password: $password) {
+      id
+      token
+    }
+  }
+`;
+
+export const SIGNUP_USER_MUTATION = gql`
+  mutation SignupUserMutation($name: String!, $email: String!, $password: String!) {
+    signupUser(name: $name, email: $email, password: $password) {
       id
       token
     }
