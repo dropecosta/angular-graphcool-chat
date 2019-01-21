9cd40667ee9a554113409497f07dc6de4b876c87
dropecosta <pedro-reis@puc.br>
2019-01-21 16:26:06 -0200

    Authguard for protected routes with CanActivateChild

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs' 
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
-import { AuthService } from '../core/services/auth.service'
-import { LoginRoutingModule } from './login-routing.module'
+import { AuthService } from '../core/services/auth.service';
+import { LoginRoutingModule } from './login-routing.module';
 
 @Injectable({
-    providedIn: LoginRoutingModule
+  providedIn: LoginRoutingModule
 })
-export class NameGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
-    constructor(
-        private authService: AuthService
-    ) { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean> {
-        return this.authService.isAuthenticated;
-    }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.isAuthenticated;
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.canActivate(route, state);
+  }
+
 }
